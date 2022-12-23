0644299e06a134d1f2c4825054bf2b52f99f4ccd
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-23 08:02:04 -0500

    Create variable size grid

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 grid = document.querySelector('.grid');
-for(i = 0; i < 16; i++){
+dimensions = 16;
+size = '50px'
+grid.setAttribute('style', `grid-template-rows: repeat(${dimensions}, ${size}); grid-template-columns: repeat(${dimensions},${size})`)
+for(i = 0; i < dimensions * dimensions; i++){
     const div = document.createElement('div');
     div.setAttribute('style', 'margin: auto')
     div.textContent = 'Hello';
