2ce06a6019f192f37c07c296fc940e5c477a3933
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-23 10:04:24 -0500

    Restructure code

--- script.js
+++ script.js
@@ -11,12 +11,11 @@ function dimIsValid(dimensions){
     return dimensions >= gridMinLimit && dimensions <= gridMaxLimit; 
 }
 
-function drawGrid(dimensions){
-    
-    const grid = document.createElement('div');
+function drawGrid(dimensions){    
+    grid = document.createElement('div');
     grid.classList.add('grid');
     container.appendChild(grid);
-    const size = maxGridSize / dimensions;
+    size = maxGridSize / dimensions;
     sizeString = `${size}px`;
     grid.setAttribute('style', `display: grid; 
                     grid-template-rows: repeat(${dimensions}, ${sizeString}); 
@@ -45,7 +44,6 @@ button.addEventListener('click', (event)=>{
     dimensions = prompt("Enter size of grid (eg. 16, 32...):");
     if(dimIsValid(dimensions)){
         drawGrid(dimensions);
-        clearGrid();
     }else{
         alert(`Size is invalid! Size must be integer between ${gridMinLimit} and ${gridMaxLimit}.`)
     }
