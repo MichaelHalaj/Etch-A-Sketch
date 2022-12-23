89bd8280efa2d89eeefe2f1368f07a5e132fcb75
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-23 09:06:05 -0500

    Grid not working properly

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 const maxGridSize = 960;
 
-function changeColor(){
-
-}
 
 container = document.querySelector('.container');
 grid = document.createElement('div');
 grid.classList.add('grid');
 container.appendChild(grid);
-dimensions = 16;
+dimensions = 10;
+
+//grid.setAttribute('style', 'border: 5px solid black;');
 
 
 size = maxGridSize / dimensions;
 sizeString = `${size}px`;
-grid.setAttribute('style', `display: grid; grid-template-rows: repeat(${dimensions}, ${sizeString}); grid-template-columns: repeat(${dimensions},${sizeString})`)
+grid.setAttribute('style', `display: grid; 
+                grid-template-rows: repeat(${dimensions}, ${sizeString}); 
+                grid-template-columns: repeat(${dimensions},${sizeString}); 
+                justify-items: stretch; 
+                align-items: stretch;
+                border: 5px solid black;
+                padding: ${maxGridSize} ${maxGridSize}`)
 for(i = 0; i < dimensions * dimensions; i++){
     const div = document.createElement('div');
-    div.setAttribute('style', 'margin: auto');
+    div.setAttribute('style', 'margin: auto; background-color: blue;');
+    div.setAttribute('id', i);
+    //div.textContent = 'Hello';
     div.classList.add('grid-item');
-    div.textContent = 'Hello';
     grid.appendChild(div);
     div.addEventListener('mouseover', (event)=>{
-        
+        div.setAttribute('style', 'background: black;');
+        console.log(event);
     });
 }
