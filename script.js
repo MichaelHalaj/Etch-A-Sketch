4e83fffb8a2984667d68952bcbd303db10a7a56b
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-23 09:18:53 -0500

    Fix styling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const maxGridSize = 960;
-
+maxGridSizeString = `${maxGridSize}px`;
 
 container = document.querySelector('.container');
 grid = document.createElement('div');
@@ -7,7 +7,7 @@ grid.classList.add('grid');
 container.appendChild(grid);
 dimensions = 10;
 
-//grid.setAttribute('style', 'border: 5px solid black;');
+
 
 
 size = maxGridSize / dimensions;
@@ -17,11 +17,10 @@ grid.setAttribute('style', `display: grid;
                 grid-template-columns: repeat(${dimensions},${sizeString}); 
                 justify-items: stretch; 
                 align-items: stretch;
-                border: 5px solid black;
-                padding: ${maxGridSize} ${maxGridSize}`)
+                `)
 for(i = 0; i < dimensions * dimensions; i++){
     const div = document.createElement('div');
-    div.setAttribute('style', 'background: gray;');
+    div.setAttribute('style', 'background: #EEE;');
     div.setAttribute('id', i);
     //div.textContent = 'Hello';
     div.classList.add('grid-item');
