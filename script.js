e175c7032a64e182b6ab488c97cacaa31d7a2ba2
Michael Halaj <michaelhalaj7@gmail.com>
2022-12-23 08:22:14 -0500

    Refactor

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const maxGridSize = 960;
 
+function changeColor(){
 
-grid = document.querySelector('.grid');
+}
+
+container = document.querySelector('.container');
+grid = document.createElement('div');
+grid.classList.add('grid');
+container.appendChild(grid);
 dimensions = 16;
 
 
 size = maxGridSize / dimensions;
 sizeString = `${size}px`;
-grid.setAttribute('style', `grid-template-rows: repeat(${dimensions}, ${sizeString}); grid-template-columns: repeat(${dimensions},${sizeString})`)
+grid.setAttribute('style', `display: grid; grid-template-rows: repeat(${dimensions}, ${sizeString}); grid-template-columns: repeat(${dimensions},${sizeString})`)
 for(i = 0; i < dimensions * dimensions; i++){
     const div = document.createElement('div');
-    div.setAttribute('style', 'margin: auto')
+    div.setAttribute('style', 'margin: auto');
+    div.classList.add('grid-item');
     div.textContent = 'Hello';
     grid.appendChild(div);
+    div.addEventListener('mouseover', (event)=>{
+        
+    });
 }
